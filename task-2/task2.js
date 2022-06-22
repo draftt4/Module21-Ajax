6ac3880cc5eade8bf99cb4a95e0fd3a072ea1e7c
Your Name <jojo11@mail.bg>
2022-06-22 21:19:28 +0300

    task2

---
+++ task-2/task2.js
@@ -0,0 +1,9 @@
+const objectTostring = {
+    name: "Anton",
+    age: 36,
+    skill:["Javascript", "HTML","CSS"],
+    salary: 80000,
+}
+
+const data = JSON.stringify(objectTostring);
+console.log(data);
