c53c7718ca21e7c25d11f56a9b221ad502dab592
Valentin Ivanov <valentinivanov@Air-Valentin.fritz.box>
2022-06-29 20:15:18 +0300

    completed

--- task-6/task6.js
+++ task-6/task6.js
@@ -2,7 +2,7 @@ let inFirst = document.querySelector('.number1').value;
 let inSecond = document.querySelector('.number2').value;
 const button = document.querySelector('.Btn');
 const contend = document.querySelector('.img');
-let data = fetch("https://picsum.photos/v2/list?page=1&limit=10");
+
 
 button.addEventListener('click',() => {
 
@@ -10,31 +10,17 @@ button.addEventListener('click',() => {
     console.log('Номер страницы вне диапазона от 1 до 10');
     }if (inSecond < 1 || inSecond > 10) {
         console.log('Лимит вне диапазона от 1 до 10');
-    } else {
-        display();
+    } else{
+        fetch("https://picsum.photos/v2/list?page=1&limit=10").then((data)=>{
+            return data.json();
+        }).then((complated) =>{
+            let data1 = '';
+            complated.map((value)=>{
+                data1+=`<div class="img">${value.id} + ${value.author}+ ${value.author}+ ${value.width}+${value.height} ${value.url}</div>`
+            });
+            contend.textContent = data1
+        }).catch((error)=>{
+            console.log('Some thing go wrong');
+        })
     }
 });
-
-function reqData () {
-    fetch(data)
-    .then(res => res.json())
-    .then(data => {
-        display(data);
-    })
-    .catch((error)=>{
-        console.error(error);
-    })
-}
-
-
-function display(data) {
-    console.log(data)
-    const ul = document.createElement('ul');
-    content.append(ul);
-    data.forEach(ele => {
-        console.log(ele);
-    });
-    const li = document.createElement('li');
-    li.textContent = `${ele.userId} ${ele.id} ${ele.title} ${ele.completed}`;
-    ul.append(li);
-}
